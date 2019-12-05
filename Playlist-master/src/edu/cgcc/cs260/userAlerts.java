bf2cb85fe839ae86bf965e494a19e14dd0ae63d9
ithenry <55816847+ithenry@users.noreply.github.com>
2019-12-04 17:25:43 -0800

    Add files via upload
    For real this time

---
+++ Playlist-master/src/edu/cgcc/cs260/userAlerts.java
@@ -0,0 +1,20 @@
+package edu.cgcc.cs260;
+
+
+public class userAlerts {
+
+    try
+
+
+     } catch {
+        {
+public void renameFailed () {
+        System.out.println("Invalid name, please enter a valid name");
+        }
+public void;
+
+    }
+
+
+}
+
