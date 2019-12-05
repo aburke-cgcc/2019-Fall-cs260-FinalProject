6e4c56ef2e11a70bd35920b44d8c5ad2155bb352
CL1304 <CL1304@1304Lab11-18.cgcc.cc.or.us>
2019-12-04 18:47:45 -0800

    update

--- Playlist-master/src/edu/cgcc/cs260/controller.java
+++ Playlist-master/src/edu/cgcc/cs260/controller.java
@@ -13,7 +13,9 @@ public class controller {
         public static void playlistTraversal(LinkedList<Song> playlist) {
             Scanner UI = new Scanner(System.in);
             printOptions menu = new printOptions();
+            
             boolean forward = true;
+            boolean backward = true;
             boolean isQuit = false;
             ListIterator<Song> listIterator = playlist.listIterator();
 
@@ -44,8 +46,9 @@ public class controller {
                 switch(choose) {
                     case 1:
                     case 2:
+                    	
+                    	
                     case 3:
-                        //Skip song
                     case 4:
                         //if there is a next song in the playlist, move forward to the next song
                         if (!forward) {
@@ -64,18 +67,41 @@ public class controller {
                         }
                         break;
 
-
                     case 5:
+                        //if there is a previous song in the playlist, move backward to the previous song
+                        if (!backward) {
+                            if (listIterator.hasPrevious())
+                                listIterator.previous();
+                            backward = true;
+                        }
+                        //if moving backward, print the song name and time
+                        if (listIterator.hasPrevious()) {
+                            System.out.println("Now playing " + listIterator.previous().toString());
+                        }
+                        //if playlist is at head, print a message
+                        else {
+                            System.out.println("You've reached the begining of it all.");
+                            backward = false;
+                        }
+                        break;
+
                     case 6:
+                    	//if quit is selected 
+                    	if (isQuit)
+                    		System.exit(0);
 
 
                 }
             }
+           
+            	
+            	
+           
 
 
             //get user input as in if playPlaylist is selected then do
             //if next node != null point to next node, else alert user something
             //call play() when pointed to next node that isn't null
         }
-
+		
 }
