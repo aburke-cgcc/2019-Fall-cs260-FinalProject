3db98869acab33d9b1e68d94b74728d4a85231ad
ithenry <55816847+ithenry@users.noreply.github.com>
2019-12-05 19:20:39 -0800

    Update controller.java
    Added cases for the rest of the cases, added comments, added Play song, stop song, delete song, previous song, and exit playlist.

--- Playlist-master/src/edu/cgcc/cs260/controller.java
+++ Playlist-master/src/edu/cgcc/cs260/controller.java
@@ -1,53 +1,90 @@
-
+/**
+ * Controls the traversal of the playlist
+ * @author Isaak Henry
+ */
 package edu.cgcc.cs260;
 
 import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Iterator;
 import java.util.ListIterator;
 
 
-public class controller {
-        //members
-
-        public static void playlistTraversal(LinkedList<Song> playlist) {
+public class Controller {
+    /**
+     * Control Playlist traversal
+     * @param playlist
+     */
+    public static void play(LinkedList<Song> playlist) {
             Scanner UI = new Scanner(System.in);
-            printOptions menu = new printOptions();
-            
+            Menu menu = new Menu();
             boolean forward = true;
-            boolean backward = true;
-            boolean isQuit = false;
+            boolean traverse = true;
             ListIterator<Song> listIterator = playlist.listIterator();
+            Song Song = new Song("Name", 0);
 
+            //Check if the playlist is empty
             if (playlist.size() == 0) {
-                System.out.println("screw off and try again");
-                //userAlerts.useralert; IF WE DECIDE TO GO THE ABSTRACT ALERTS ROUTE
+                System.out.println("No playlist found.");
                 return;
             } else {
-                System.out.println("NOW PLAYING" + listIterator.next().toString());
-                printOptions.printMenu();
+                //if the playlist is not empty, print the menu
+                System.out.println("Choose an option");
+                Menu.printMenu();
             }
+
             // while not choosing to quit, we will scan the menu
             // line by line to find the option we want
-
-            while (!isQuit) {
+            while (traverse) {
                 int choose = UI.nextInt();
                 UI.nextLine();
 
 
             /*
-            1 - Play playlist
-            2 - Stop song
+            1 - Play Song
+            2 - Stop Song
             3 - Delete song
             4 - Skip Forward
             5 - Skip Backward
-            6 - Quit Playlist
+            6 - Play Playlist
+            7 - Stop Playlist
+            8 - Delete Playlist
+            9 - Quit Playlist
              */
-                switch(choose) {
+                switch(choose){
                     case 1:
+                        //Play Song
+                        if(!forward){
+                            if(listIterator.hasNext()){
+                                listIterator.next();
+                            }
+                            forward = true;
+                        }
+                        if(listIterator.hasNext()){
+                            System.out.println("Playing" + listIterator.next().toString());
+                        } else {
+                            System.out.println("The playlist is now over");
+                            forward = false;
+                        }
+                        break;
                     case 2:
+                        //Stop Song
+                        Song.stopSong();
+
                     case 3:
+			//delete current song
+                        //Check if playlist has any songs
+                        if (playlist.size() > 0) {
+			    //if it does, remove the song currently playing
+                            listIterator.remove();
+		            //play the next song
+                            if(listIterator.hasNext()){
+                                System.out.println("Playing" + listIterator.next());
+                            } else if(listIterator.hasPrevious()){
+                                System.out.println("Playing" + listIterator.previous());
+                            }
+                        }
                     case 4:
+                        //Skip Forward
                         //if there is a next song in the playlist, move forward to the next song
                         if (!forward) {
                             if (listIterator.hasNext())
@@ -64,42 +101,39 @@ public class controller {
                             forward = false;
                         }
                         break;
-
                     case 5:
-                        //if there is a previous song in the playlist, move backward to the previous song
-                        if (!backward) {
-                            if (listIterator.hasPrevious())
+			//Previous Song
+			//check if there is a previous node
+                        if(forward){
+                            if (listIterator.hasPrevious()) {
+				//goto previous node
                                 listIterator.previous();
-                            backward = true;
+                            }
+                            forward = false;
                         }
-                        //if moving backward, print the song name and time
-                        if (listIterator.hasPrevious()) {
-                            System.out.println("Now playing " + listIterator.previous().toString());
-                        }
-                        //if playlist is at head, print a message
-                        else {
-                            System.out.println("You've reached the begining of it all.");
-                            backward = false;
+			//play previous song
+                        if(listIterator.hasPrevious()){
+                            System.out.println("Playing" + listIterator.previous().toString());
+                        } else {
+			    //If at beginning of playlist, alert user
+                            System.out.println("At beginning of playlist");
+                            forward = true;
                         }
                         break;
-
                     case 6:
-                    	//if quit is selected 
-                    	if (isQuit)
-                    		System.exit(0);
+				//play playlist
+                    case 7:
+				//stop playlist
+                    case 8:
+				//delete playlist
 
-
+                        listIterator.remove();
+                        break;
+                    case 9:
+                        System.out.println("Goodbye");
+                        traverse = false;
+                        System.exit(0);
                 }
-            }
-           
-            	
-            	
-           
-
-
-            //get user input as in if playPlaylist is selected then do
-            //if next node != null point to next node, else alert user something
-            //call play() when pointed to next node that isn't null
+	    }
         }
-		
 }
